ae6896b2333ad7739c1dde65fb85c653f2ff7aa1
Suada Haji <suada.haji@andela.com>
2018-05-17 09:11:39 +0300

    Implement MD5 hash algorithm

---
+++ src/MD5Sample.java
@@ -0,0 +1,62 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+
+public class MD5Sample {
+    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchProviderException
+    {
+        String passwordToHash = "secure_password";
+        byte[] salt = getSalt();
+
+        String securePassword = getSecurePassword(passwordToHash, salt);
+        System.out.println(securePassword);
+
+        String regeneratedPassowrdToVerify = getSecurePassword(passwordToHash, salt);
+        System.out.println(regeneratedPassowrdToVerify);
+    }
+
+    private static String getSecurePassword(String passwordToHash, byte[] salt)
+    {
+        String generatedPassword = null;
+        try {
+            // Create MessageDigest instance for MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            //Add password bytes to digest
+            md.update(salt);
+
+            //Get the hash's bytes
+            byte[] bytes = md.digest(passwordToHash.getBytes());
+
+            //Since this bytes[] has bytes in decimal format, convert it to hexadecimal format
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
+            {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            //Get complete hashed password in hex format
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
+
+    //Add salt
+    private static byte[] getSalt() throws NoSuchAlgorithmException, NoSuchProviderException
+    {
+        //Always use a SecureRandom generator
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "SUN");
+
+        //Create array for salt
+        byte[] salt = new byte[16];
+
+        //Get a random salt
+        sr.nextBytes(salt);
+
+        //return salt
+        return salt;
+    }
+}
